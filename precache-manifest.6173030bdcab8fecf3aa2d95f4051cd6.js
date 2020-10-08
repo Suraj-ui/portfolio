ad3e230843baf95c0d3f39ebd917d9068c400b9b
Suraj-ui <sp990047@gmail.com>
2020-10-08 20:54:30 +0530

    Updates

---
+++ precache-manifest.6173030bdcab8fecf3aa2d95f4051cd6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e6b3798d5a0b20f49c1a1ec6c2ebf2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "444606f1addfcfbc0a6e",
+    "url": "/portfolio/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "c684221e1497638ddf9c",
+    "url": "/portfolio/static/css/main.8f7f4c55.chunk.css"
+  },
+  {
+    "revision": "444606f1addfcfbc0a6e",
+    "url": "/portfolio/static/js/2.6ade26d3.chunk.js"
+  },
+  {
+    "revision": "0a0d166b4064dde4d07ca121e187fa82",
+    "url": "/portfolio/static/js/2.6ade26d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c684221e1497638ddf9c",
+    "url": "/portfolio/static/js/main.5cb26684.chunk.js"
+  },
+  {
+    "revision": "c052b00bb4f40f1c0e5f",
+    "url": "/portfolio/static/js/runtime-main.8d39d6be.js"
+  },
+  {
+    "revision": "ae26131bf7a14ee3f08e34e53afb0d7e",
+    "url": "/portfolio/static/media/Ab1.ae26131b.jpg"
+  },
+  {
+    "revision": "52def69b50176601c8f2bcf226b1c045",
+    "url": "/portfolio/static/media/Android.52def69b.jpg"
+  },
+  {
+    "revision": "779b5892e720cc1526f389adf33bd170",
+    "url": "/portfolio/static/media/AppD.779b5892.png"
+  },
+  {
+    "revision": "b32e2c766d9958a2f166bb75dcb49ab1",
+    "url": "/portfolio/static/media/DigitalM.b32e2c76.png"
+  },
+  {
+    "revision": "987903c2edf1d7a9975011af68a58d60",
+    "url": "/portfolio/static/media/Home.987903c2.png"
+  },
+  {
+    "revision": "45392f04a1e15fcbd53610a66723ae61",
+    "url": "/portfolio/static/media/SoftD.45392f04.png"
+  },
+  {
+    "revision": "57e269d772efb557362226367e9e2a63",
+    "url": "/portfolio/static/media/WebD.57e269d7.jpeg"
+  },
+  {
+    "revision": "67fb93dab937e9464e89280bfd15e105",
+    "url": "/portfolio/static/media/about.67fb93da.png"
+  },
+  {
+    "revision": "a3a9ed6642dfac81d83a6af66f810c56",
+    "url": "/portfolio/static/media/ios.a3a9ed66.png"
+  }
+]);
